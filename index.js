69a3cb021ac51ef254c5f3665b72b105c1583f21
PhoenixCrimson <33698159+PhoenixCrimson@users.noreply.github.com>
2018-02-25 20:45:18 +0100

    dd

--- index.js
+++ index.js
@@ -2,32 +2,37 @@ var Discord = require("discord.js")
 var mysql = require('mysql')
 var bot = new Discord.Client()
 var config = require("./config.json")
-var connection = mysql.createConnection({
+var db_config = {
     host: config.host,
     user: config.User,
     password: config.Databasepass,
     database: config.database
-})
-var handleKFDisconnect = function() {
-    kfdb.on('error', function(err) {
-        if (!err.fatal) {
-            return;
-        }
-        if (err.code !== 'PROTOCOL_CONNECTION_LOST') {
-            console.log("PROTOCOL_CONNECTION_LOST");
-            throw err;
-        }
-        log.error("The database is error:" + err.stack);
+}
+var connection = mysql.createConnection(db_config)
 
-        kfdb = mysql.createConnection(kf_config);
 
-        console.log("kfid");
-
-        console.log(kfdb);
-        handleKFDisconnect();
+function handleDisconnect() {
+    connection = mysql.createConnection(db_config); // Recreate the connection, since
+                                                    // the old one cannot be reused.
+  
+    connection.connect(function(err) {              // The server is either down
+      if(err) {                                     // or restarting (takes a while sometimes).
+        console.log('error when connecting to db:', err);
+        setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+      }                                     // to avoid a hot loop, and to allow our node script to
+    });                                     // process asynchronous requests in the meantime.
+                                            // If you're also serving http, display a 503 error.
+    connection.on('error', function(err) {
+      console.log('db error', err);
+      if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+        handleDisconnect();                         // lost due to either server restart, or a
+      } else {                                      // connnection idle timeout (the wait_timeout
+        console.log(err);                                  // server variable configures this)
+      }
     });
-   };
-   handleKFDisconnect();
+  }
+  
+  handleDisconnect();
 
 connection.connect()
 
